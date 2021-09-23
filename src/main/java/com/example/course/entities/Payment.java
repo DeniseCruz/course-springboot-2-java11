1c5f6292b72ea7f51fc3d601a4054ddb08af9a00
Denise Cruz <denisecz@gmail.com>
2021-09-23 15:39:21 -0300

    post/delete de usuario

--- src/main/java/com/example/course/entities/Payment.java
+++ src/main/java/com/example/course/entities/Payment.java
@@ -12,6 +12,8 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name = "tb_payment")
 public class Payment implements Serializable{
@@ -22,9 +24,11 @@ public class Payment implements Serializable{
 	private Long id;
 	private Instant moment;
 	
+	///jsonIgnore para evitar loop infinito com order
+	@JsonIgnore
 	@OneToOne
 	@MapsId
-    private Order order;
+	private Order order;
     
     @SuppressWarnings("unused")
 	private Payment() {
--- src/main/java/com/example/course/resources/UserResource.java
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -1,17 +1,23 @@
 package com.example.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.course.entities.User;
 import com.example.course.services.UserService;
-//// controlador REST
+//// controlador REST ....endpoint
 
 @RestController
 @RequestMapping(value = "/users")
@@ -34,5 +40,21 @@ public class UserResource {
 	 public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj) ;
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		/////return ResponseEntity.ok().body(obj) ; return 200 ok
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri(); 
+		return ResponseEntity.created(uri).body(obj) ; /// retorna ok 201 created
+		
+	}
+	
+	//// banco de dados só deixa deletar usuario sem pedido
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build(); ///retorna 204
 	}
 }
